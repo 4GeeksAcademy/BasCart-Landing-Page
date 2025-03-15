93845437a5992567287cff25e910aa5aeab453f3
BasCart <b.cartagena.paredes@gmail.com>
2025-03-15 02:42:05 +0000

    modified home

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-
+import { Nav } from "./navbar";
+import { Jumbotron } from "./jumbotron";
+import { Card } from "./card";
+import { Footer } from "./footer";
+ 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Nav/>
+			<div className="mx-auto " style={{width: "70%"}}>
+				<Jumbotron />
+				<div className="container-fluid">
+					<div className="row justify-content-center gap-2">
+						<Card className="col-3" />
+						<Card className="col-3" />
+						<Card className="col-3" />
+						<Card className="col-3" />
+					</div>
+				</div>	
+			</div>
+			<Footer/>
 		</div>
-	);
-};
-
+	)
+}
 export default Home;
