2c8b764848a0bd6b16ca9ab4d78cc67a3593d5cc
BasCart <b.cartagena.paredes@gmail.com>
2025-03-15 02:38:56 +0000

    adding footer

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer =() =>{
+    return (
+        <div className="container-fluid mt-3 text-center bg-dark text-light" >
+            <p className=" p-5" >Copyright © Your Website 2025</p>
+        </div>
+    )
+} 
