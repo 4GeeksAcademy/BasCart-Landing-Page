9e2a18a4ee0539448190f16ec549c72d203a2111
BasCart <b.cartagena.paredes@gmail.com>
2025-03-15 02:39:42 +0000

    adding jumbotron

---
+++ src/js/components/jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Jumbotron =() => {
+    return(
+        <div className="my-5 pt-2 px-4 pb-5 " style={{backgroundColor: '#c8d8e0'}}>
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque officiis quidem id error vero ex, eveniet laborum sed sit velit fuga nesciunt, odio, obcaecati eos! Vero asperiores dicta soluta distinctio?</p>
+            <button type="button" className="btn btn-primary">Call to action!</button>
+        </div>
+    )
+}
